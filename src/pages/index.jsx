3db884f7bcc5684dde37086fa42895efad19c4e6
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-08-15 16:03:06 +0700

    filter data dan pengenalan useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,23 @@
-/* eslint-disable react/jsx-key */
-import Article from "../components/Article";
-import posts from "../posts.json"
+import { useState } from 'react';
+import Article from '../components/Article';
+import posts from '../posts.json';
 
 function Homepage() {
+  const [search, setSearch] = useState("")
+
+  const changeSearch = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value)
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date}/>
+      <div>
+        Cari Article : <input onChange={changeSearch}></input>
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map(({ title, tags, date }, index) => (
+        <Article {...{ title, tags, date }} key={index} />
       ))}
     </>
   );
