e3f373cd608fd71a6b5eca317cd7d9a776e13953
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-08-15 20:10:20 +0700

    Dasar state management dengan useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
-import Home from './pages/index'
+import './App.css';
+import Home from './pages/index';
+import { GlobalContext } from './contex';
 
 function App() {
+  const user = {
+    username: 'Febrian',
+  };
+
   return (
-    <div className='App'>
-      <Home/>
+    <div className="App">
+      <GlobalContext.Provider value={user}>
+        <Home />
+      </GlobalContext.Provider>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { GlobalContext } from "../contex";
 const ArticleStatus = ({ isNew }) => {
   return isNew && <span>-- Baru!!</span>;
 };
 
-const NewArticle = () => {
-  return <span>---BARU!!</span>
-}
-
 export default function Article(props) {
+  const user = useContext(GlobalContext)
   return (
     <>
       <h3>{props.title}</h3>
       <small>
         Date : {props.date}, tags: {props.tags.join(', ')}{' '}
         <ArticleStatus isNew={props.isNew} />
-        {props.isNew && <NewArticle />}
       </small>
+      <div>
+        <small>Ditulis oleh {user.username}</small>
+      </div>
     </>
   );
 }
