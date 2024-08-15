1214544883189f4230b35459392d97895b562d6e
pebritisnasetiawan <pebritisnasetiawan@gmail.com>
2024-08-15 20:39:23 +0700

    Layouting and children routes

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from 'react-router-dom';
+
+function RootLayout() {
+  return (
+    <>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      <p />
+      <Outlet/>
+    </>
+  );
+}
+
+export default RootLayout;
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,14 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from '../layouts/RootLayout';
+
 import Home from '../pages';
 import About from '../pages/About';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
   },
 ]);
